ce1efe14dd9edd82907992b42b5bc2aa123f0c95
janhavichauhan <143059176+janhavichauhan@users.noreply.github.com>
2023-11-26 15:19:41 +0530

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Image from './components/image';
+import Image from './components/Image';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
